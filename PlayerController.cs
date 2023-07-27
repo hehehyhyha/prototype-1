d86f37938d66fffee566b33379f0caa4619ef1df
dababyletgo <belloolajide1@gmail.com>
2023-07-27 22:18:10 +0100

    Added veriable

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwordInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwordInput= Input.GetAxis("Vertical");
 
         //Move the vehicle forword
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwordInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }

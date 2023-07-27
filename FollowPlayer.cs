36303ad20eeae968ef6e4187316d2096e462397c
dababyletgo <belloolajide1@gmail.com>
2023-07-27 21:53:17 +0100

    Added Vector3

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -14,6 +14,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }

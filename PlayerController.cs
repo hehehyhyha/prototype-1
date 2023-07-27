70e123208137bcfc539adbbfd55b1ee9ecb8cb5d
dababyletgo <belloolajide1@gmail.com>
2023-07-27 22:32:58 +0100

    cleaned up code

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwordInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwordInput;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +20,9 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwordInput= Input.GetAxis("Vertical");
-
-        //Move the vehicle forword
+        //Move the vehicle forword based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwordInput);
+        // Rotates car based on input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }

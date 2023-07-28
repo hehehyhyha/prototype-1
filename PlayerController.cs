a2851fc39f72142c4d5f152667ed13e5e69e08f8
dababyletgo <belloolajide1@gmail.com>
2023-07-28 18:50:02 +0100

    added inputID

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwordInput;
+    public string inputID;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwordInput= Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwordInput= Input.GetAxis("Vertical" + inputID);
         //Move the vehicle forword based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwordInput);
         // Rotates car based on input

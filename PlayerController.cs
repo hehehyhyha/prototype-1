fc07dff9001c8198c424cc7f60cfed61a43fac22
dababyletgo <belloolajide1@gmail.com>
2023-07-28 19:12:45 +0100

    loloolo

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwordInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     public string inputID;
 
     // Start is called before the first frame update
@@ -25,5 +28,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwordInput);
         // Rotates car based on input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
